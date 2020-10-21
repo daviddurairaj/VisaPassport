21407516493a52df3010854bf6b74bf2c5fc1bc3
David <david_durairaj@yahoo.com>
2020-10-21 15:54:05 -0500

    added call to visa api, not working with 401 error

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,18 @@
 from flask import Flask, jsonify, make_response
 import requests
+from requests.auth import HTTPBasicAuth
+
 
 app = Flask(__name__)
 
 @app.route('/hello')
 def get_issues():
-	r = requests.get('https://api.github.com/repos/walmartlabs/thorax/issues')
-	
-	for x in r.json():
-		print('state: ', x['state'], 'number: ', x['number'], 'title: ', x['title'] )
+    
+	r = requests.get(
+	'https://sandbox.api.visa.com/dcas/cardservices/v1/cards/8d212293-c6bc-4738-afaf-bc0ae5456df5/transactions',
+    cert=('path to cert file', 'path to key file'),
+	auth=HTTPBasicAuth('JZZYCI15ZPIO305G77W721_0tp8rS5WCU3WvsCAQhKFmVXT', 'Z3dnOkLy1R8IvMsHeH0EZ3c0uskvwHXOPi')
+	)
+
+	print(r.json())
 	return "success"
